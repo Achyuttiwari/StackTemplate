6246e32b2c5ecb43c6c74b9b2313be5cf5756699
dbc2201 <divyanshb22@gmail.com>
2020-01-31 12:38:49 +0530

    take input from the user and store it into the choice variable

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -15,11 +15,13 @@ public class Main {
     public static void main(String[] args) {
         MyStack<Integer> stack = new MyStack<>();
         final Scanner scanner = new Scanner(System.in);
+        int choice = 0;
         while (true) {
             System.out.println("Press 1 to \"Push\" a node at the top.");
             System.out.println("Press 2 to \"Pop\" a node from the top.");
             System.out.println("Press 3 to \"Peek\" at the node at the top.");
             System.out.println("Press 4 to exit.");
+            choice = scanner.nextInt();
         }
     }
 }
