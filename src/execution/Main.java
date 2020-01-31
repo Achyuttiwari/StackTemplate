25c0987c47874c9207fdd96e1280c01ed5e1d64e
dbc2201 <divyanshb22@gmail.com>
2020-01-31 12:42:19 +0530

    add default case

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -33,6 +33,8 @@ public class Main {
                     break;
                 case 4: /*EXIT*/
                     System.exit(0);
+                default:
+                    System.out.println("Please enter a valid choice!");
             }
         }
     }
