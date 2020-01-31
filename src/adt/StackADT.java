8ed2fbb05d7db2dc6bc6127f61ca1f6bd6f0ca62
dbc2201 <divyanshb22@gmail.com>
2020-01-31 11:04:50 +0530

    add JavaDoc comments for the peek() method

--- src/adt/StackADT.java
+++ src/adt/StackADT.java
@@ -4,4 +4,10 @@ package adt;
  * It is the ADT specification for a sample stack implementation.
  */
 public interface StackADT<E> {
+    /**
+     * This method will return the data of the node that is placed at the "top" of the stack.
+     *
+     * @return the data of the node at the top of the stack, NoSuchElementException if the stack was empty.
+     */
+    E peek();
 }
