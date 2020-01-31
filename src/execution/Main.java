ff3927b1d56eb7a9837f809930fc9e3e633a45b3
dbc2201 <divyanshb22@gmail.com>
2020-01-31 12:37:57 +0530

    add menu print statements

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -12,5 +12,11 @@ import definition.MyStack;
 public class Main {
     public static void main(String[] args) {
         MyStack<Integer> stack = new MyStack<>();
+        while (true) {
+            System.out.println("Press 1 to \"Push\" a node at the top.");
+            System.out.println("Press 2 to \"Pop\" a node from the top.");
+            System.out.println("Press 3 to \"Peek\" at the node at the top.");
+            System.out.println("Press 4 to exit.");
+        }
     }
 }
